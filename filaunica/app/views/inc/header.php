69bdda6e67ef918d49c213e81d6743b1117daff2
jeandrei <jeandreiwalter@gmail.com>
2022-11-17 17:39:41 +0000

    novo view do admins

--- filaunica/app/views/inc/header.php
+++ filaunica/app/views/inc/header.php
@@ -10,7 +10,7 @@
    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.min.css">
    
    <!--Font Awesome CDN-->
-   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
    
    <!--CSS MIDIFICAÇÕES SOBESCREVER Botstrap-->
    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">   
